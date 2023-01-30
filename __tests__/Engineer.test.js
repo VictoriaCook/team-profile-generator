88927474980a057cac0be3236b72ab182b256588
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-01-30 16:16:05 +1100

    added example test function for Engineer class

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,7 @@
+// Example of test function for Engineer
+
+// test("getRole() should return \"Engineer\"", () => {
+//     const testValue = "Engineer";
+//     const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+//     expect(e.getRole()).toBe(testValue);
+//   });
