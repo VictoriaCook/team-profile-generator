42b889c6f556def8eee0afab1b5d1b9e05b53f78
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-02-02 15:08:48 +1100

    set up generate page functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const generateTeam = require("./src/generate");
 
 // First series of prompts (manager)
 
