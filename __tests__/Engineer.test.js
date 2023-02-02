6cd3b1a1eb8d2882f4ba7aae985305c1862d0982
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-02-02 18:41:36 +1100

    added tests for subclasses and fixed Github undefined error test fail

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,19 @@
-// Example of test function for Engineer
+const Engineer = require("../lib/Engineer")
 
-// test("getRole() should return \"Engineer\"", () => {
-//     const testValue = "Engineer";
-//     const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
-//     expect(e.getRole()).toBe(testValue);
-//   });
+it("Can set Github username via constructor" , () => {
+    const testValue = "testGithub"
+    const e = new Engineer("Sarah", 2, "test@test.com" , testValue);
+    expect(e.github).toBe(testValue);
+});
+
+it("getRole() should return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Sam", 1, "test@test.com", testValue);
+    expect(e.getRole()).toBe(testValue);
+  });
+
+it("Can get Github username via getGithub()", () => {
+    const testValue = "testGithub";
+    const e = new Engineer("Jenny", 2, "test@test.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+});
