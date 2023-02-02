8763bd81aaf8dddbb83a5faf2514f10840cc8070
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-02-02 18:43:50 +1100

    deleted extra comments

--- index.js
+++ index.js
@@ -152,38 +152,3 @@ const addIntern = () => {
 function createTeam() {
   fs.writeFileSync("./dist/output.html", generateTeam(teamData), "utf-8");
 }
-
-// const generateMarkdown = require('./generateMarkdown');
-
-// // Function to write README file
-// function writeToFile(fileName, data) {
-//     fs.writeFile(fileName, data, (err) => 
-//     err ? console.log(err) : console.log('Successfully created README.md file')
-//     );
-// }
-
-// // Function to initialize app
-// function init() {
-//     const prompt = inquirer.createPromptModule();
-//     prompt(questions)
-//     .then((answers) => {
-//         const mdPageContent = generateMarkdown(answers);
-//         writeToFile('./output/README.md', mdPageContent);
-//     });
-// }
-
-// Function call to initialize app
-// init();
-
-// const employee = new Employee('Jane', '123abc', 'jane@ihaveajob.com');
-
-// console.log(employee)
-
-// employee.getName()
-
-// prompt for employee - get name, id, email
-// then type of employee etc
-// if conditions to run different prompt modules depending on user selection
-// arrays must contain the same data type only e.g. all strings
-// arrays of objects in this case
-// use getRole function to check what type of employee we have inside the array
