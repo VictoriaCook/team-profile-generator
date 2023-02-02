5d5f054979deb385ad6229422775981dea80b00c
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-02-02 18:51:31 +1100

    fixed href to output.css in generate.js file

--- src/generate.js
+++ src/generate.js
@@ -95,7 +95,7 @@ const generateTeam = (team) => {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="output.css">
         <title>Document</title>
     </head>
     <body>
